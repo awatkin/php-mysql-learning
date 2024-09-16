ad835ce221016c21927058a09f66f84d11abf9cc
adam <adam.watkin@utcleeds.co.uk>
2024-09-16 08:55:52 +0100

    updated files moved to git to carry on elsewhere

---
+++ basics/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> This is my page </title>
+    <link rel="stylesheet" href="styles.css">
+
+    <?php
+    $cookie = 23;
+    $price = 2;
+    $name = "Steven";
+
+    /* this is a
+    multiline quote
+    which can carry on
+    */
+    ?>
+</head>
+
+<body>
+
+<?php  # Essential tag for including a PHP block
+echo "<p id='mypara'>First bit of code ";
+echo "Hello World! </p>";  # echo used to output content including html tags
+echo "Final price is $finalprice";
+echo $finalprice; //this is a comment too
+
+
+# closing of the php tag
+?>
+</body>
+</html>
+
